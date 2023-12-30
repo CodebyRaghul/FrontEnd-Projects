0e0c8e3c61563a4f51c3e28d1499993bf9132e56
CodebyRaghul <raghuldharman11>
2023-12-30 13:46:51 +0530

    create a submit button

--- linkedinclone/src/Components/LoginComponents.jsx
+++ linkedinclone/src/Components/LoginComponents.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "../Sass/LoginComponent.scss"
+import { LoginAPI } from '../api/AuthAPI'
 
 function LoginComponents() {
+
+  const login = () =>{
+    console.log( LoginAPI() );
+  }
+
   return (
     <div>
         <h1>LoginComponents</h1>
+        <button onClick={login} className='login-btn'> Login into LinkedIn</button>
     </div>
   )
 }
